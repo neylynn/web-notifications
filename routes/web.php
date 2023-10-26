91fdd8790a46a63cdcc1be8d90ace70b0a9ca547
neylynn <neylynn97@gmail.com>
2023-10-26 15:57:51 +0630

    update some parts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('test', function () {
+    event(new App\Events\MessageSent('websolutionstuff_team'));
+    return "Event has been sent!";
 });
